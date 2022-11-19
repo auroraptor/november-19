2a714345f49af84ebb6ba9426cbdbc9b7c5cbaf4
auroraptor <interestingbutno@yandex.ru>
2022-11-19 16:05:45 +0300

    feat: timer

--- 1-task/index.js
+++ 1-task/index.js
@@ -4,15 +4,31 @@ const timerEl = document.querySelector('span');
 
 // Напишите реализацию createTimerAnimator
 // который будет анимировать timerEl
+
 const createTimerAnimator = () => {
-  return (seconds) => {};
+  return (seconds) => {
+      const timer = setInterval(() => {
+      const hh = Math.floor(seconds / 3600).toString().padStart(2, 0);
+      const mm = Math.floor((seconds % 3600) / 60).toString().padStart(2, 0);
+      const ss = Math.floor(seconds % 60).toString().padStart(2, 0);
+
+      timerEl.textContent = `${hh}:${mm}:${ss}`;
+
+      seconds--;
+
+      if (seconds < 0) {
+        clearInterval(timer);
+      }
+    }, 1000);
+  };
 };
 
 const animateTimer = createTimerAnimator();
 
-inputEl.addEventListener('input', () => {
+inputEl.addEventListener('input', (e) => {
   // Очистите input так, чтобы в значении
   // оставались только числа
+  inputEl.value = inputEl.value.replace(/[^0-9]/, "");
 });
 
 buttonEl.addEventListener('click', () => {
