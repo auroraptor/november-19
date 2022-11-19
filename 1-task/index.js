1cccf8c645c78b74fd7521b2435101d1ce1b4dd4
auroraptor <interestingbutno@yandex.ru>
2022-11-19 18:21:56 +0300

    fix

--- 1-task/index.js
+++ 1-task/index.js
@@ -1,40 +1,52 @@
-const inputEl = document.querySelector('input');
-const buttonEl = document.querySelector('button');
-const timerEl = document.querySelector('span');
-
-// Напишите реализацию createTimerAnimator
-// который будет анимировать timerEl
+const inputEl = document.querySelector("input");
+const buttonEl = document.querySelector("button");
+const timerEl = document.querySelector("span");
 
 const createTimerAnimator = () => {
   return (seconds) => {
-      const timer = setInterval(() => {
-      const hh = Math.floor(seconds / 3600).toString().padStart(2, 0);
-      const mm = Math.floor((seconds % 3600) / 60).toString().padStart(2, 0);
-      const ss = Math.floor(seconds % 60).toString().padStart(2, 0);
+    const countDown = new Date().getTime() + seconds * 1000 + 1000;
 
-      timerEl.textContent = `${hh}:${mm}:${ss}`;
+    const timer = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = countDown - now;
 
-      seconds--;
+      timerEl.textContent = getFormatTime(distance);
 
-      if (seconds < 0) {
+      if (distance < 0) {
         clearInterval(timer);
+        timerEl.textContent = "⌛️";
       }
     }, 1000);
   };
 };
 
+const getFormatTime = (milliseconds) => {
+  const hours = Math.floor(milliseconds  / (1000 * 60 * 60))
+    .toString()
+    .padStart(2, 0);
+  const minutes = Math.floor((milliseconds % (1000 * 60 * 60)) / (1000 * 60))
+    .toString()
+    .padStart(2, 0);
+  const seconds = Math.floor((milliseconds % (1000 * 60)) / 1000)
+    .toString()
+    .padStart(2, 0);
+
+  return `${hours}:${minutes}:${seconds}`;
+};
+
 const animateTimer = createTimerAnimator();
 
-inputEl.addEventListener('input', (e) => {
-  // Очистите input так, чтобы в значении
-  // оставались только числа
+inputEl.addEventListener("input", (e) => {
   inputEl.value = inputEl.value.replace(/[^0-9]/, "");
 });
 
-buttonEl.addEventListener('click', () => {
+buttonEl.addEventListener("click", () => {
   const seconds = Number(inputEl.value);
+  const milliseconds = seconds * 1000;
+
+  timerEl.textContent = getFormatTime(milliseconds);
 
   animateTimer(seconds);
 
-  inputEl.value = '';
+  inputEl.value = "";
 });
